fbbd17108b7b107f2e88c25b5d3d53f98d39e832
Mandy Sou <z5521445@ad.unsw.edu.au>
2025-08-01 06:53:13 +1000

    Move data fetch for cancer umap to servce

--- explore-classifiers/src/Components/Umap.tsx
+++ explore-classifiers/src/Components/Umap.tsx
@@ -3,6 +3,7 @@ import Plot from 'react-plotly.js';
 import LoadingAnimation from './Animations/LoadingAnimation';
 import { Scatter3dData } from 'plotly.js';
 import { buildUmapData } from '../Helpers/buildUmapData';
+import { getCancerUMAP } from '../Service/getCancerUMAPData';
 
 interface UmapProps {
     cancerType: string;
@@ -11,22 +12,12 @@ interface UmapProps {
 const Umap = ({ cancerType }: UmapProps) => {
     const [umap, setUmap] = useState<any>(null);
 
-    // Fetch UMAP data based on the selected cancer type
     useEffect(() => {
-        fetch(`/cancer_type/${cancerType}`)
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-                return response.json();
-            })
+        getCancerUMAP(cancerType)
             .then((data) => {
-                const plot = JSON.parse(data.plot);
-                setUmap(plot);
+                setUmap(data);
             })
-            .catch((error) => {
-                console.error('Error fetching UMAP: ', error);
-            })
+            .catch(err => console.error(err));
     }, [cancerType]);
 
     const data: Partial<Scatter3dData>[] | undefined = umap ? buildUmapData(umap.data) : undefined;
--- explore-classifiers/src/Service/getCancerHireachyData.tsx
+++ explore-classifiers/src/Service/getCancerHireachyData.tsx
@@ -7,7 +7,6 @@ export type CancerTypeData = {
 
 export const getCancerHireachy = async (): Promise<CancerTypeData[]> => {
     const response = await fetch('/cancer/cancer_structure');
-    fetch(`/cancer/cancer_structure`)
     if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
     const data = await response.json();
     return data.cancer_types;
